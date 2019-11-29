7011ded56ad8d4837da849348b6fd5baedb153c8
Nguyễn Duy Toản <nguyenduytoanbkdn@gmail.com>
2019-11-29 17:04:52 +0700

    Update myJS.js

--- jQuery/VALIDATE_DUY TOAN/js/myJS.js
+++ jQuery/VALIDATE_DUY TOAN/js/myJS.js
@@ -34,16 +34,20 @@ $(document).ready(function () {
             },
             "company":{
                 required:true,
-                minlength: 6
+                minlength: 6,
+                formatName:true
             },
             "firstName":{
-                required:true
+                required:true,
+                formatName:true
             },
             "lastName":{
-                required:true
+                required:true,
+                formatName:true
             },
             "phone":{
-                required:true
+                required:true,
+                formatPhone: true
             }
         },
         messages: {
@@ -61,20 +65,30 @@ $(document).ready(function () {
             },
             "company":{
                 required:"Company must be filled",
-                minlength:" Comany must be at least 6 characters in length"
+                minlength:" Comany must be at least 6 characters in length",
+                formatName:"Company's name only contain character"
             },
             "firstName":{
-                required:"First Name must be filled"
+                required:"First Name must be filled",
+                formatName: "Name only contain character"
             },
             "lastName":{
-                required:"Last Name must be filled"
+                required:"Last Name must be filled",
+                formatName: "Name only contain character"
             },
             "phone":{
-                required:"Phone must be filled"
+                required:"Phone must be filled",
+                formatPhone:"Please enter a valid phone number "
             }
            
         }
     });
+    $.validator.addMethod("formatName",function(value){
+        return /^[A-Za-z]/.test(value);
+    });
+    $.validator.addMethod("formatPhone",function(value){
+        return /^[0-9]{3}-[0-9]{3}-[0-9]{4}/.test(value);
+    })
    
     $(':radio').change(function(){
         var isCh = $("input[type='radio']:checked").val();
